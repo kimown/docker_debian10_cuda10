bcc34701c06505464456c7d0e8abc94b07f7adde
kimown <kimown@163.com>
2020-10-25 20:34:58 +0800

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,3 @@
+# get your download link by clicking button Download (2.0 GB)  : https://developer.nvidia.com/cuda-10.0-download-archive?target_os=Linux&target_arch=x86_64&target_distro=Ubuntu&target_version=1804&target_type=runfilelocal
+wget https://us.download.nvidia.com/tesla/418.116.00/NVIDIA-Linux-x86_64-418.116.00.run
+wget -O cuda_10.0.130_410.48_linux.run https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda_10.0.130_410.48_linux
